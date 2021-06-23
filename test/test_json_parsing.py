6a127cff2e3cb872060320c3c0dfed0f0e6790c5
ka3a4ook <89052585288@mail.ru>
2021-06-23 18:15:09 +0300

    Программа, которая парсит переменную json_text.

---
+++ test/test_json_parsing.py
@@ -0,0 +1,9 @@
+import json
+
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},' \
+            '{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+
+
+def test_json_parsing():
+    data = json.loads(json_text)
+    print(data['messages'][1]['message'])
